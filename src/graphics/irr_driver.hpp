9bfe430bc4a54e55198f5ce09cec415b6c435615
Vincent Lejeune <vljn@ovi.com>
2014-12-05 18:52:21 +0100

    Implement DFG LUT for phong specular

--- src/graphics/irr_driver.hpp
+++ src/graphics/irr_driver.hpp
@@ -255,6 +255,7 @@ private:
 public:
     GLuint SkyboxCubeMap;
     GLuint SkyboxSpecularProbe;
+    GLuint DFG_LUT;
     /** A simple class to store video resolutions. */
     class VideoMode
     {
